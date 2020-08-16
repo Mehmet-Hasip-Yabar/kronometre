dfc071d7d042ff954f3e9678cbdc9a24516590d4
Mehmet Hasip <66031216+Mehmet-Hasip-Yabar@users.noreply.github.com>
2020-08-16 22:52:50 +0300

    kronometre
    kısa mesafe koşusu için basit bir kronometre.

---
+++ kronometre/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int sayac;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+            if (sayac<=10)
+            {
+                //timer1.Stop();
+                this.BackColor = Color.YellowGreen;
+            }
+            if (sayac==11)
+            {
+                this.BackColor = Color.Orange;
+            }
+            if (sayac == 21)
+            {
+                this.BackColor = Color.Red;
+                sayac = 0;
+                timer1.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
